eb0021b53ff61100780dbb04c4a42de1a35b47f5
Xian Qiu <qiuxan@gmail.com>
2018-07-16 06:24:56 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,15 +14,21 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-//		$topics = Topic::paginate();
-////		return view('topics.index', compact('topics'));
-///
+//	public function index()
+//	{
+////		$topics = Topic::paginate();
+//////		return view('topics.index', compact('topics'));
+/////
+//
+//        $topics = Topic::with('user', 'category')->paginate(30);
+//        return view('topics.index', compact('topics'));
+//	}
 
-        $topics = Topic::with('user', 'category')->paginate(30);
+    public function index(Request $request, Topic $topic)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
-	}
+    }
 
     public function show(Topic $topic)
     {
