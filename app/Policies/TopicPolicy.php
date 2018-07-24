cc2e0e435032084034487a4110e22012cd16681b
Xian Qiu <qiuxan@gmail.com>
2018-07-24 03:20:52 +0000

    Author can delete topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -6,17 +6,14 @@ use App\Models\User;
 use App\Models\Topic;
 
 class TopicPolicy extends Policy
-
-
 {
     public function update(User $user, Topic $topic)
     {
-         return $topic->user_id == $user->id;
-
+        return $user->isAuthorOf($topic);
     }
 
     public function destroy(User $user, Topic $topic)
     {
-        return true;
+        return $user->isAuthorOf($topic);
     }
 }
