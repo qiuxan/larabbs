4a09239674869cbe2fb5729db470a0794bdedb29
Xian Qiu <qiuxan@gmail.com>
2018-07-26 12:20:02 +0000

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -30,5 +30,10 @@ class ReplyObserver
         $topic->user->notify(new TopicReplied($reply));
     }
 
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
+    }
+
 
 }
